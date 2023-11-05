17ce9d652f2366dacde24feca5c5001cf4bee0a3
GB-MyLang <ghanashyam.b@mylang.in>
2023-11-05 17:22:28 +0530

    basic html

--- energy_analysis/urls.py
+++ energy_analysis/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("dashboard/", views.dashboard),
+    path("profile/", views.profile),
+    path("form/", views.form_submit),
 ]
--- energy_analysis/views.py
+++ energy_analysis/views.py
@@ -9,3 +9,19 @@ def dashboard(request):
         return render(
             request, "dashboard.html", context={"message": "Success"}, status=201
         )
+
+
+@login_required(login_url="/auth/login/")
+def profile(request):
+    if request.method == "GET":
+        return render(
+            request, "profile_page.html", context={"message": "Success"}, status=201
+        )
+
+
+@login_required(login_url="/auth/login/")
+def form_submit(request):
+    if request.method == "GET":
+        return render(
+            request, "user_input.html", context={"message": "Success"}, status=201
+        )
